2a708ea7173734a59c73ffb81390f87ca5cc80dc
Raymico Fuji <129006790+PistolManiac22@users.noreply.github.com>
2023-10-27 06:07:08 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+let nilai = 85;
+
+if (nilai <= 100 && nilai >= 85) {
+    console.log("Nilai kamu A");
+}
+else if (nilai <= 84 && nilai >= 75) {
+    console.log("Nilai Kamu B");
+}
+else if (nilai <= 74 && nilai >= 60) {
+    console.log("Nilai Kamu C")
+}
+else if (nilai <= 59 && nilai >= 0) {
+    console.log("Nilai Kamu D")
+}
+else {
+    console.log("Error")
+}
